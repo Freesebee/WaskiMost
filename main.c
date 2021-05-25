aa063f31a386d705f42997e03b9ca72dc4ea726d
FreeseBee <ajaku@DESKTOP-JTEOCI7>
2021-05-25 19:49:41 +0200

    Podstawowy algorytm wariantu b)

--- main.c
+++ main.c
@@ -1,20 +1,85 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void* CarGoesBrrrrr(void* arg)
+//TODO: Realizacja b) - funkcje:
+/*
+
+pthread_cond_signal(&brdigeCondition, &bridgeMutex);
+
+pthread_cond_wait(&brdigeCondition);
+Wykonuje:
+1. pthread_mutex_unlock(&bridgeMutex);
+2. Czeka na sygnał od pthread_cond_signal(&brdigeCondition, &bridgeMutex);
+3. pthread_mutex_lock(&bridgeMutex);
+
+pthread_cond_broadcast(&brdigeCondition, &bridgeMutex);
+Wykonuje to samo co pthread_cond_wait(,) lecz dla wszystkich wątków oczekujących na sygnał
+*/
+
+pthread_mutex_t bridgeMutex;
+pthread_cond_t brdigeCondition;
+int bridge = 0;
+
+/*!
+@param _carNumber wskażnik do numeru pojazdu przydzielonego przy tworzeniu
+*/
+void* CarMovement(void* _carNumber)
 {
-    int carNumber = *(int*)arg;
+    int carNumber = *(int*)_carNumber;
 
     //konieczna dealokacja, gdyż CreateCars() alokuje pamięć specjalnie dla parametru
-    free(arg);
+    free(_carNumber);
 
-    printf("Car number %d goes brrrr\n", carNumber);
+    for(int i = 0; i < 2; i++) //TODO: Zastąp nieskończoną pętlą
+    {
+        printf("#%d w A\n", carNumber);
+
+        pthread_mutex_lock(&bridgeMutex);
+
+        while(bridge != 0)
+        {
+            printf("#%d czeka, bo #%d na moście\n",carNumber,bridge);
+            pthread_cond_wait(&brdigeCondition, &bridgeMutex);
+        }
+
+        bridge = carNumber;
+        printf("Info od #%d: #%d na moście\n",carNumber,bridge);
+        bridge = 0;
+        pthread_mutex_unlock(&bridgeMutex);
+        pthread_cond_signal(&brdigeCondition);
+//        sleep(1);
+
+        printf("#%d w B\n", carNumber);
+
+        pthread_mutex_lock(&bridgeMutex);
+
+        while(bridge != 0)
+        {
+            printf("#%d czeka, bo #%d na moście\n",carNumber,bridge);
+            pthread_cond_wait(&brdigeCondition, &bridgeMutex);
+        }
+
+        bridge = carNumber;
+        printf("Info od #%d: #%d na moście\n",carNumber,bridge);
+        bridge = 0;
+        pthread_mutex_unlock(&bridgeMutex);
+        pthread_cond_signal(&brdigeCondition);
+//        sleep(1);
+
+    }
+
+    printf("#%d w A\n", carNumber);
+
     return NULL;
 }
 
-///@param count Liczba pojazdów do utworzenia jako wątki
-///@returns (pthread_t*) Tablica wątków
+/*!
+@param count liczba pojazdów do utworzenia jako wątki
+@returns tablica utworzonych wątków
+@details tworzy tablicę adresów wątków oraz przydziela każdemu numer pojazdu jako atrybut
+*/
 pthread_t* CreateCars(int count)
 {
     pthread_t* carsArray = (pthread_t*)malloc(sizeof(pthread_t) * count);
@@ -26,7 +91,7 @@ pthread_t* CreateCars(int count)
         int* carNumber = (int*)malloc(sizeof (int));
         *carNumber = i+1;
 
-        if(pthread_create(&carsArray[i], NULL, CarGoesBrrrrr, carNumber) != 0)
+        if(pthread_create(&carsArray[i], NULL, CarMovement, carNumber) != 0)
         {
             perror("Nie udało się utworzyć wątku");
         }
@@ -35,6 +100,11 @@ pthread_t* CreateCars(int count)
     return carsArray;
 }
 
+/*!
+@param carsArray tablica wątków
+@param count rozmiar tablicy wątków
+@details przyłącza utworzone wątki znajduące się w podanej tablicy
+*/
 void DestroyCars(pthread_t* carsArray, int count)
 {
     for (int i = 0; i < count; ++i)
@@ -46,14 +116,17 @@ void DestroyCars(pthread_t* carsArray, int count)
     }
 }
 
-/// @param argc Liczba argumentów programu
-/// @param argv Tablica argumentow programu
-/// @returns Liczba pojazdów do utworzenia
+/*!
+@param argc liczba argumentów programu
+@param argv tablica argumentow programu
+@returns liczba pojazdów do utworzenia
+@details sprawdza poprawność podanych i odczytuje wprowadzone argumenty
+*/
 int GetCarCount(int argc, char **argv)
 {
     if(argc != 2)
     {
-        perror("Niepoprawna liczba argumentow\nSyntax: liczba_pojazdów\n");
+        perror("Niepoprawna liczba argumentow\nSyntax: tightbridge liczba_pojazdów\n");
         exit(EXIT_FAILURE);
     }
 
@@ -61,11 +134,37 @@ int GetCarCount(int argc, char **argv)
 
     if(N <= 0)
     {
-        printf("Liczba pojazdów musi być dodatnia\n");
+        perror("Liczba pojazdów musi być dodatnia\n");
         exit(EXIT_FAILURE);
     }
 
     return N;
+}
+
+/*!
+@param cityACarsCount liczba pojazdów znajdujących się w mieście A
+@param cityBCarsCount liczba pojazdów znajdujących się w mieście B
+@param queueACarsCount liczba pojazdów jadących z miasta A, czekających na przejazd
+@param queueBCarsCount liczba pojazdów jadących z miasta B, czekających na przejazd
+@param direction kierunek ruchu pojazdu przejeżdżającego przez most
+@param carOnBridgeNumber numer pojazdu przejeżdżającego przez most
+@details wypisuje aktualny stan ruchu przez most
+*/
+void PrintCurrentState(int cityACarsCount,
+                       int cityBCarsCount,
+                       int queueACarsCount,
+                       int queueBCarsCount,
+                       char* direction,
+                       int carOnBridgeNumber)
+{
+    printf("A-%d %d>>> [%s %d %s] <<<%d %d-B\n",
+           cityACarsCount,
+           queueACarsCount,
+           direction,
+           carOnBridgeNumber,
+           direction,
+           queueBCarsCount,
+           cityBCarsCount);
 }
 
 void Algorytm_MutexySemafory()
@@ -116,13 +215,38 @@ void Algorytm_ZmienneWarunkowe()
 //    end; {CZYTELNIA}
 }
 
-int main(int argc, char** argv)
+void CrossBridgeVersionA(int carCount)
 {
-    int carCount = GetCarCount(argc, argv);
-    printf("Liczba pojazdow: %d\n", carCount);
+    pthread_mutex_init(&bridgeMutex, NULL);
 
     pthread_t* carsArray = CreateCars(carCount);
+
+    //TODO: Reszta kodu wersji a)
+
+    pthread_mutex_destroy(&bridgeMutex);
+
+    free(carsArray);
+}
+
+void CrossBridgeVersionB(int carCount)
+{
+    pthread_cond_init(&brdigeCondition, NULL);
+
+    pthread_t* carsArray = CreateCars(carCount);
+
+    //Pomocnicze kolekcje
+//    int* aCityQueue = (int*)calloc(carCount,sizeof(int));
+//    int* bCityQueue = (int*)calloc(carCount,sizeof(int));
+
     DestroyCars(carsArray, carCount);
+
+    free(carsArray);
+}
+
+int main(int argc, char** argv)
+{
+//    CrossBridgeVersionA(GetCarCount(argc, argv));
+    CrossBridgeVersionB(GetCarCount(argc, argv));
 
     return 0;
 }
