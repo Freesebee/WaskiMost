3820ac04781baecaf35d3bdd179d8d7e84fa168f
FreeseBee <ajaku@DESKTOP-JTEOCI7>
2021-06-01 18:45:09 +0200

    Implementacja kolejki i dodanie zawijania regionów kodu

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
-
-//makro opisujące prawa dostępu do pliku kolejki FIFO
-#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#include <string.h>
+#include <errno.h>
 
 //TODO: Realizacja b) - funkcje:
 /*
@@ -23,12 +23,150 @@ pthread_cond_broadcast(&brdigeCondition, &bridgeMutex);
 Wykonuje to samo co pthread_cond_wait(,) lecz dla wszystkich wątków oczekujących na sygnał
 */
 
+//region Implementacja kolejki
+
+/*!@details Element kolejki będący elementem listy jednokierunkowej*/
+typedef struct queueElement{
+    int carNuber;
+    struct queueElement* next;
+} QUEUE_ELEM;
+
+/*!
+ * @details Funkcja usuwająca numer pojazdu z kolejki
+ * @param QUEUE_ELEM* adres pierwszego elementu kolejki
+ * @return (int) pierwszy element kolejki lub -1 jeżeli kolejka jest pusta
+ */
+int Peek(QUEUE_ELEM* head)
+{
+    if(head == NULL) return -1;
+    return head->carNuber;
+}
+
+/*!
+ * @details Funkcja dodająca numer pojazdu do kolejki
+ * @param QUEUE_ELEM** adres wskaźnika do pierwszego elementu kolejki
+ * @return 0 w przypadku sukcesu, -1 w przypadku błędu (errno = ENOMEM)
+ */
+int Enqueue(QUEUE_ELEM **head, int carNumber)
+{
+    QUEUE_ELEM* newElem = (QUEUE_ELEM *)malloc(sizeof(QUEUE_ELEM));
+
+    if(newElem == NULL)
+    {
+        errno = ENOMEM;
+        return -1;
+    }
+
+    newElem->next = NULL;
+    newElem->carNuber = carNumber;
+
+    if(*head == NULL)
+    {
+        *head = newElem;
+        return 0;
+    }
+
+    QUEUE_ELEM* iterator = *head;
+
+    while(iterator->next != NULL) iterator = iterator->next;
+
+    iterator->next = newElem;
+
+    return 0;
+}
+
+/*!
+ * @details Funkcja usuwająca numer pojazdu z kolejki
+ * @param QUEUE_ELEM** adres wskaźnika do pierwszego elementu kolejki
+ * @return (int) pierwszy element kolejki lub -1 jeżeli kolejka jest pusta
+ */
+int Dequeue(QUEUE_ELEM **head)
+{
+    if(*head == NULL) return -1;
+    int carNumber = (*head)->carNuber;
+    QUEUE_ELEM* first = *head;
+    *head = (*head)->next;
+    free(first);
+    return carNumber;
+}
+
+/*!
+ * @details Funkcja zwracająca rozmiar kolejki
+ * @param QUEUE_ELEM* adres pierwszego elementu kolejki
+ * @return (int) rozmiar kolejki
+ */
+int GetQueueLenght(QUEUE_ELEM* head)
+{
+    int length = 0;
+    QUEUE_ELEM * iterator = head;
+    while(iterator != NULL)
+    {
+        length++;
+        iterator = iterator->next;
+    }
+    return length;
+}
+
+//endregion
+
+//region Zmienne globalne
+/*!@details Opisuje prawa dostępu do pliku kolejki FIFO*/
 pthread_mutex_t bridgeMutex;
+
+/*!@details Zmienna warunkowa sygnalizująca wątkom dostęp do zasobu*/
 pthread_cond_t brdigeCondition;
+
+/*!@details Pierwszy element kolejki*/
+QUEUE_ELEM *queueA, *queueB;
+
+/*!@details Numer pojazdu aktualnie przejeżdżającego przez most*/
 int selectedCar = 0;
+
+/*!@details liczba pojazdów aktualnie znajdujących się w mieście A*/
+int cityCountA = 0;
+
+/*!@details liczba pojazdów aktualnie znajdujących się w mieście B*/
+int cityCountB = 0;
+
+//endregion
+
+//region Podstawowe funkcje
+/*!
+@param cityA liczba pojazdów znajdujących się w mieście A
+@param cityB liczba pojazdów znajdujących się w mieście B
+@param queueA liczba pojazdów jadących z miasta A, czekających na przejazd
+@param queueB liczba pojazdów jadących z miasta B, czekających na przejazd
+@param fromCityA kierunek ruchu pojazdu przejeżdżającego przez most
+@param carOnBridge numer pojazdu przejeżdżającego przez most
+@details wypisuje aktualny stan ruchu przez most
+*/
+void PrintCurrentState(int cityA, int cityB, char fromCityA, int carInfo,int carOnBridge )
+{
+    char* direction;
+
+    if (fromCityA == 0) //jedzie z miasta B
+    {
+       direction = "<<";
+    }
+    else //jedzie z miasta A
+    {
+        direction = ">>";
+    }
+
+    printf("[#%d] A-%d %d>>> [%s %d %s] <<<%d %d-B\n",
+           carInfo,
+           cityA,
+           GetQueueLenght(queueA),
+           direction,
+           carOnBridge,
+           direction,
+           GetQueueLenght(queueB),
+           cityB);
+}
 
 /*!
 @param _carNumber wskażnik do numeru pojazdu przydzielonego przy tworzeniu
+@details Funkcja wykonywana przez każdy utworzony wątek
 */
 void* CarMovement(void* _carNumber)
 {
@@ -37,22 +175,22 @@ void* CarMovement(void* _carNumber)
     //konieczna dealokacja, gdyż CreateCars() alokuje pamięć specjalnie dla parametru
     free(_carNumber);
 
-    for/*(;;)*/(int i = 0; i < 2; i++) //TODO: Zastąp nieskończoną pętlą
+    for/*(;;)*/(int i = 0; i < 3; i++) //TODO: Zastąp nieskończoną pętlą
     {
         printf("#%d w A\n", carNumber);
 
         pthread_mutex_lock(&bridgeMutex);
 
-        while(selectedCar != 0 && selectedCar != carNumber)
+        while(selectedCar != -1 /*&& selectedCar != carNumber*/)
         {
             printf("#%d czeka, bo #%d na moście\n",carNumber,selectedCar);
             pthread_cond_wait(&brdigeCondition, &bridgeMutex);
         }
 
         selectedCar = carNumber;
-        printf("Info od #%d: #%d na moście\n", carNumber, selectedCar);
+        PrintCurrentState(0,0,1,carNumber, selectedCar);
 
-        selectedCar = 0;
+        selectedCar = -1;
         pthread_mutex_unlock(&bridgeMutex);
         pthread_cond_broadcast(&brdigeCondition);
 
@@ -60,16 +198,16 @@ void* CarMovement(void* _carNumber)
 
         pthread_mutex_lock(&bridgeMutex);
 
-        while(selectedCar != 0 && selectedCar != carNumber)
+        while(selectedCar != -1 /*&& selectedCar != carNumber*/)
         {
             printf("#%d czeka, bo #%d na moście\n",carNumber,selectedCar);
             pthread_cond_wait(&brdigeCondition, &bridgeMutex);
         }
 
         selectedCar = carNumber;
-        printf("Info od #%d: #%d na moście\n", carNumber, selectedCar);
+        PrintCurrentState(0,0,0,carNumber,selectedCar);
 
-        selectedCar = 0;
+        selectedCar = -1;
         pthread_mutex_unlock(&bridgeMutex);
         pthread_cond_broadcast(&brdigeCondition);
     }
@@ -80,9 +218,9 @@ void* CarMovement(void* _carNumber)
 }
 
 /*!
-@param count liczba pojazdów do utworzenia jako wątki
+@param count dodatnia liczba pojazdów do utworzenia jako wątki
 @returns tablica utworzonych wątków
-@details tworzy tablicę adresów wątków oraz przydziela każdemu numer pojazdu jako atrybut
+@details tworzy tablicę adresów wątków oraz przydziela każdemu numer pojazdu <0; maxInt>
 */
 pthread_t* CreateCars(int count)
 {
@@ -90,10 +228,10 @@ pthread_t* CreateCars(int count)
 
     for (int i = 0; i < count; ++i)
     {
-        //wątki nie startują zaraz po utworzeniu, więc każdy musi odwoływać się
-        //do adresu, który nie ulegnie zmianie w czasie przed wystartowaniem
+//        wątki nie startują zaraz po utworzeniu, więc każdy musi odwoływać się
+//        do adresu, który nie ulegnie zmianie w czasie przed wystartowaniem
         int* carNumber = (int*)malloc(sizeof (int));
-        *carNumber = i+1;
+        *carNumber = i;
 
         if(pthread_create(&carsArray[i], NULL, CarMovement, carNumber) != 0)
         {
@@ -145,108 +283,13 @@ int GetCarCount(int argc, char **argv)
     return N;
 }
 
-/*!
-@param cityA liczba pojazdów znajdujących się w mieście A
-@param cityB liczba pojazdów znajdujących się w mieście B
-@param queueA liczba pojazdów jadących z miasta A, czekających na przejazd
-@param queueB liczba pojazdów jadących z miasta B, czekających na przejazd
-@param direction kierunek ruchu pojazdu przejeżdżającego przez most
-@param carOnBridge numer pojazdu przejeżdżającego przez most
-@details wypisuje aktualny stan ruchu przez most
-*/
-void PrintCurrentState(int cityA, int cityB, int queueA, int queueB, char* direction, int carOnBridge)
-{
-    printf("A-%d %d>>> [%s %d %s] <<<%d %d-B\n",
-           cityA,
-           queueA,
-           direction,
-           carOnBridge,
-           direction,
-           queueB,
-           cityB);
-}
-
-void Algorytm_MutexySemafory()
-{
-
-}
-
-void Algorytm_ZmienneWarunkowe()
-{
-//    monitor CZYTELNIA;
-//    var czyta, pisze: integer;
-//    CZYTELNICY, PISARZE: condition;
-//
-//    export procedure POCZ.CZYTANIA;
-//    begin
-//    if not empty(PISARZE) or (pisze > 0) then
-//    wait CZYTELNICY);
-//    czyta := czyta + l
-//    end; {POCZ.CZYTANIA}
-//
-//    export procedure KON_CZYTANIA;
-//    begin
-//    czyta := czyta - 1;
-//    if czyta = O then
-//    signal(PISARZE)
-//    end; {KON_CZYTANIA}
-//
-//    export procedure POCZ_PISANIA;
-//    begin
-//    if czyta+pisze > O then wait(PISARZE);
-//    pisze := l
-//    end; {POCZ_PISANIA}
-
-//    export procedure KON_PISANIA;
-//    begin
-//    pisze := 0;
-//    if empty(CZYTELNICY) then
-//    signal(PISARZE)
-//    else
-//    repeat
-//    signal(CZYTELNICY)
-//    until empty(CZYTELNICY)
-//    end; {KON_PISANIA}
-//
-//    begin
-//    czyta := 0;
-//    pisze := O
-//    end; {CZYTELNIA}
-}
-
-void CrossBridgeVersionA(int carCount)
-{
-    pthread_mutex_init(&bridgeMutex, NULL);
-
-    pthread_t* carsArray = CreateCars(carCount);
-
-    //TODO: Reszta kodu wersji a)
-
-    pthread_mutex_destroy(&bridgeMutex);
-
-    free(carsArray);
-}
+//endregion
 
 void CrossBridgeVersionB(int carCount)
 {
     pthread_cond_init(&brdigeCondition, NULL);
 
     pthread_t* carsArray = CreateCars(carCount);
-
-    mkfifo("FIFO_A", FILE_MODE);
-    mkfifo("FIFO_B", FILE_MODE);
-
-    int pid = fork();
-
-    // PROCES MACIERZYSTY:
-    int writefd = open("FIFO_A", O_WRONLY, 0);
-    int readfd = open("FIFO_B", O_RDONLY, 0);
-    //ProceduraOjca(...);,
-
-    // PROCES POTOMNY:
-    readfd = open("FIFO_A", O_RDONLY, 0);
-    writefd = open("FIFO_B", O_WRONLY, 0);
-    //ProceduraSyna(...);
 
     DestroyCars(carsArray, carCount);
 
@@ -255,8 +298,15 @@ void CrossBridgeVersionB(int carCount)
 
 int main(int argc, char** argv)
 {
-//    CrossBridgeVersionA(GetCarCount(argc, argv));
-    CrossBridgeVersionB(GetCarCount(argc, argv));
+    int carCount = GetCarCount(argc, argv);
+
+    pthread_cond_init(&brdigeCondition, NULL);
+
+    pthread_t* carsArray = CreateCars(carCount);
+
+    DestroyCars(carsArray, carCount);
+
+    free(carsArray);
 
     return 0;
 }
